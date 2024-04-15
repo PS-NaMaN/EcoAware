4b930f288823cb3c96d3948258d6a66fe6c29bed
SanshrayGit <sanshray.c@outlook.com>
2024-04-15 17:52:37 +0530

    added file structure

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+'''@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        num1 = float(request.form['num3'])
+        num2 = float(request.form['num4'])
+        result = num1 + num2
+        return render_template('result.html', result=result)
+'''
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+ 
+@app.route('/loginaction', methods=['GET', 'POST'])
+def loginaction():
+    
+        num1 = float(request.form['num3'])
+        num2 = float(request.form['num4'])
+        result = num1 + num2
+        return render_template('result.html', result=result)
+
+ 
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8008)
